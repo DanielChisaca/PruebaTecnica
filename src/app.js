7b0b70eeb0c33dc9d387b1b2e93d30c53684d8d2
Daniel Chisaca <danielchisaca@gmail.com>
2023-04-29 22:05:55 -0500

    Creación Swagger

--- src/app.js
+++ src/app.js
@@ -3,6 +3,11 @@ const app = express();
 const productsRoutes = require('./routes/productsRoutes');
 const authRoutes = require('./routes/authRoutes');
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+
 app.use(express.json());
 app.use('/api', productsRoutes);
 app.use('/api', authRoutes);
